b7ea8c5707ae4769191b5a93701e9fbe242d5916
sasamile <nspes2020@gmail.com>
2025-07-13 22:53:51 -0500

    fix:update histoty cittizen officer

--- src/main.ts
+++ src/main.ts
@@ -47,7 +47,7 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  const port = process.env.PORT ?? 3001;
+  const port = process.env.PORT ?? 3000;
   await app.listen(port);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
